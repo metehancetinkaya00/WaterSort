fb4f201b8403a9cd96c69eb2860b650d1b7e3b6f
metehancetinkaya00 <metehancetinkaya00@gmail.com>
2026-05-14 17:26:03 +0300

    Update LevelData.cs

--- WaterSort/Assets/Scripts/LevelData.cs
+++ WaterSort/Assets/Scripts/LevelData.cs
@@ -6,14 +6,14 @@ public class LevelData : ScriptableObject
     [System.Serializable]
     public class BottleData
     {
-        [Tooltip("Alt'tan üste sýrayla renkler (max 4). Boþ þiþe için boþ býrak.")]
+
         public Color[] colors = new Color[4];
 
-        [Tooltip("Kaç renk dolu? (0-4)")]
+      
         [Range(0, 4)]
         public int numberOfColors = 4;
 
-        [Tooltip("Dünya koordinatýnda pozisyon")]
+     
         public Vector2 position;
     }
 
